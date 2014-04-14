28688988ba2554db1904fbc61145d135b2b8d33a
psachin <iclcoolster@gmail.com>
2014-04-14 15:30:17 +0530

    Subject: South advanced : Using default values.
    Description:

--- southtut/models.py
+++ southtut/models.py
@@ -7,5 +7,6 @@ class Knight(models.Model):
     name = models.CharField(max_length=100)
     of_the_round_table = models.BooleanField()
     dances_whenever_able = models.BooleanField()
+    shrubberies = models.IntegerField(null=False)
         
         
