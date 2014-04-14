e5a91dedcc3d67a6f6100ac52224c7c438e46de1
psachin <iclcoolster@gmail.com>
2014-04-14 15:05:24 +0530

    Subject: Added another field 'dance_whenever_able'
    Description:
    
    Notes:
    - If the model is been changed(new column has been added), apply it using:
      python manage.py schemamigration southtut --auto
      python manage.py migrate southtut

--- southtut/models.py
+++ southtut/models.py
@@ -6,5 +6,6 @@ class Knight(models.Model):
     """
     name = models.CharField(max_length=100)
     of_the_round_table = models.BooleanField()
+    dances_whenever_able = models.BooleanField()
         
         
