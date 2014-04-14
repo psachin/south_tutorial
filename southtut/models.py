607520569d9356364255129b6b991296bf066745
psachin <iclcoolster@gmail.com>
2014-04-14 15:34:46 +0530

    Subject: Applying 'unique'
    Description:

--- southtut/models.py
+++ southtut/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Knight(models.Model):
     """Features of a Knight
     """
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     of_the_round_table = models.BooleanField()
     dances_whenever_able = models.BooleanField()
     shrubberies = models.IntegerField(null=False)
